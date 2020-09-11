8b91a3d29916001c9b1779999da5f1f0c02bd2f2
DeVincenzoNicole <devincenzo.nicole@hotmail.com>
2020-09-11 14:52:54 -0300

    Corrección de errores

--- enviar.php
+++ enviar.php
@@ -13,7 +13,7 @@ $mensaje .= "Su correo es " . $mail . "\r\n";
 $mensaje .= "Mensaje: " . $_POST['message'] . "\r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'devincenzo.nicole@hotmail.com'
+$para = 'devincenzo.nicole@hotmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
