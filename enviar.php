f156e2a75b3256d1acafe3f601f99014db9801c1
DeVincenzoNicole <64378602+DeVincenzoNicole@users.noreply.github.com>
2020-09-14 11:54:19 -0300

    Prueba cambios

--- enviar.php
+++ enviar.php
@@ -3,18 +3,16 @@ $nombre = $_POST['name'];
 $mail = $_POST['email'];
 $mensaje = $_POST['message'];
 
-$header = "De: " . $mail . "\r\n";
-$header .= "Content-Type: text/plain";
+$header = "Nuevo mensaje de mi portfolio";
 
 $mensajeCompleto = "Este mensaje fue enviado por " . $nombre . "\r\n";
 $mensajeCompleto .= "Su correo es " . $mail . "\r\n";
 $mensajeCompleto .= "Mensaje: " . $mensaje . "\r\n";
-$mensajeCompleto .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'devincenzo.nicole@hotmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
-mail($para, $asunto, utf8_decode($mensajeCompleto), $header);
+mail($para, $asunto, $mensajeCompleto, $header);
 
 header("Location:index.html");
 ?>
