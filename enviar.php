ae83573d9dd49a81cc34572175220929824e54b2
DeVincenzoNicole <64378602+DeVincenzoNicole@users.noreply.github.com>
2020-09-14 11:13:46 -0300

    Prueba funcionamiento 1

--- enviar.php
+++ enviar.php
@@ -4,8 +4,6 @@ $mail = $_POST['email'];
 $mensaje = $_POST['message'];
 
 $header = 'From: ' . $mail . "\r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
-$header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . "\r\n";
